c7b91658a032f8e41c61801a527ed637e05122f7
Matthew Brown <lokislayer@gmail.com>
2018-12-31 01:12:02 -0600

    Refactored
    I completely refactored this class. I went from a while loop to do while loop plus took out all the static strings to its own class to store them.

--- GenesysRoller/Program.cs
+++ GenesysRoller/Program.cs
@@ -5,55 +5,39 @@ namespace GenesysRollerCore
     public class Program
     {
         static void Main(string[] args)
-        {
-            const string about = "Genesys Python Dice Roller by Matthew Brown, @2018";
-            const string question1 = "How many boost dice? (return for 0) => ";
-            const string question2 = "How many setback dice (return for 0) => ";
-            const string question3 = "How many Ability dice (return for 0) => ";
-            const string question4 = "How many Difficulty dice (return for 0) => ";
-            const string question5 = "How many Proficiency dice (return for 0) => ";
-            const string question6 = "How many Challenge dice (return for 0) => ";
-
-            Console.WriteLine(about);
-            Console.WriteLine("Genesys RPG created by Fantasy Flight Games @2018");
-
-            // BOOST DICE
-            var blue = QuestionAnswer(question1);            
-            // SETBACK DICE
-            var black = QuestionAnswer(question2);
-            // ABILITY DICE
-            var green = QuestionAnswer(question3);
-            // DIFFICULTY DICE
-            var purple = QuestionAnswer(question4);
-            // PROFICIENCY DICE
-            var yellow = QuestionAnswer(question5);
-            // CHALLENGE DICE
-            var red = QuestionAnswer(question6);
-            
-            // 
-            while (true)
+        {            
+            bool answered = false;
+            do 
             {
+                Console.WriteLine(Questions.about);
+                Console.WriteLine(Questions.copyright);
+                // BOOST DICE
+                var blue = QuestionAnswer(Questions.question1);            
+                // SETBACK DICE
+                var black = QuestionAnswer(Questions.question2);
+                // ABILITY DICE
+                var green = QuestionAnswer(Questions.question3);
+                // DIFFICULTY DICE
+                var purple = QuestionAnswer(Questions.question4);
+                // PROFICIENCY DICE
+                var yellow = QuestionAnswer(Questions.question5);
+                // CHALLENGE DICE
+                var red = QuestionAnswer(Questions.question6);
+                // Take the dice given and display it to the screen
                 if (blue + black + green + purple + yellow + red != 0)
                 {
-                    var diceRolled = new Roller(blue, black, green, purple, yellow, red);
+                    var diceRolled = new Roller(blue,black,green,purple, yellow, red);
                     diceRolled.DisplayResults();
                     break;
                 }
-                // Tell the user that you can not roll nothing
-                Console.WriteLine("Oops. I see nothing to roll. Try again?");
-                // BOOST DICE
-                blue = QuestionAnswer(question1);            
-                // SETBACK DICE
-                black = QuestionAnswer(question2);
-                // ABILITY DICE
-                green = QuestionAnswer(question3);
-                // DIFFICULTY DICE
-                purple = QuestionAnswer(question4);
-                // PROFICIENCY DICE
-                yellow = QuestionAnswer(question5);
-                // CHALLENGE DICE
-                red = QuestionAnswer(question6);
-            }
+                else 
+                {
+                    Console.WriteLine("There is no dice to be rolled. Please add some dice to be rolled.");                    System.Console.WriteLine();
+                    System.Console.WriteLine();
+                    answered = true;
+                }
+
+            }while(answered);
         }
 
         static int QuestionAnswer(string question)
