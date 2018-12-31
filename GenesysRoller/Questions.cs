dd55f8bc46388515f9c97385c2d7ac263d482611
Matthew Brown <lokislayer@gmail.com>
2018-12-31 01:10:53 -0600

    Static Content

---
+++ GenesysRoller/Questions.cs
@@ -0,0 +1,14 @@
+namespace GenesysRollerCore
+{
+    public class Questions
+    {
+       public readonly static string about = "Genesys Python Dice Roller by Matthew Brown, @2018";
+       public readonly static string copyright = "Genesys RPG created by Fantasy Flight Games @2018";
+       public readonly static string question1 = "How many boost dice? (return for 0) => ";
+       public readonly static string question2 = "How many setback dice (return for 0) => ";
+       public readonly static string question3 = "How many Ability dice (return for 0) => ";
+       public readonly static string question4 = "How many Difficulty dice (return for 0) => ";
+       public readonly static string question5 = "How many Proficiency dice (return for 0) => ";
+       public readonly static string question6 = "How many Challenge dice (return for 0) => ";
+    }
+}
